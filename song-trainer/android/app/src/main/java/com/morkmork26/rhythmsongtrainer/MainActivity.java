527a966630364396f381310bed97cec708c57774
morkmork26 <159151337+morkmork26@users.noreply.github.com>
2026-09-07 05:02:58 +0800

    Declare Media3 opt-in at Android entry point

--- song-trainer/android/app/src/main/java/com/morkmork26/rhythmsongtrainer/MainActivity.java
+++ song-trainer/android/app/src/main/java/com/morkmork26/rhythmsongtrainer/MainActivity.java
@@ -1,8 +1,10 @@
 package com.morkmork26.rhythmsongtrainer;
 
 import android.os.Bundle;
+import androidx.media3.common.util.UnstableApi;
 import com.getcapacitor.BridgeActivity;
 
+@UnstableApi
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
